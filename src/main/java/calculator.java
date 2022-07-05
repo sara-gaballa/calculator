3ec85cc61e061e6a67fd9921dfec69214646186a
sara-gaballa <96298154+sara-gaballa@users.noreply.github.com>
2022-07-05 20:11:55 +0200

    update

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -1,3 +1,51 @@
+import com.google.common.base.Strings;
+
+import java.util.Scanner;
+
 public class calculator {
-    
+    private final String name;
+    public calculator(String name) {
+        if(Strings.isNullOrEmpty(name))
+        this.name = "standared";
+        else{
+            this.name=name;
+        }
+    }
+    public int add(int x, int y){
+        if(x<0||y<0)
+        throw new RuntimeException("wrong input");
+        else{
+            return x+y;
+        }
+    }
+    public int sub(int x, int y){
+        return x-y;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter ur name:");
+        String s=sc.nextLine();
+        calculator cal=new calculator(s);
+
+        System.out.println("User name: "+cal.name);
+        System.out.println("plz type the operation ^^ >> (add,sub)");
+        String op=sc.nextLine();
+        if(op.equals("add")){
+            System.out.println("enter the numbers");
+            int x= sc.nextInt();
+            int y= sc.nextInt();
+            System.out.println(cal.add(x,y));
+        }
+        else if(op.equals("sub")){
+            System.out.println("enter the numbers");
+            int x= sc.nextInt();
+            int y= sc.nextInt();
+            System.out.println(cal.sub(x,y));
+        }
+        else{
+            System.out.println("error");
+        }
+
+    }
 }
---
+++ src/test/java/calculatorTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class calculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void sub() {
+    }
+}
